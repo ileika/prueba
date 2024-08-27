3508aa0a760f7c305f4f8ad97ceceb7f7d8e9dd9
ileika <leidykarinaruiz@gmail.com>
2024-08-27 19:04:18 +0200

    sum of 2 numbers

---
+++ ProjectFinal/ProjectFinal/ejercicio.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int a, b;
+
+	cout << " Ingrese numero 1 : " << endl;
+	cin >> a;
+	cout << " Ingrese numero 2 : " << endl;
+	cin >> b;
+
+	cout << "El resultado de la suma es : " << a + b;
+
+	return 0;
+}
